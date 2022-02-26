fca9f42d32caa0a43fb2fd42bcf6d2fc3df26676
juanse1080 <juanmarcon1080@gmail.com>
2022-02-26 10:54:32 -0500

    add hooks for controller components

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,6 @@
+export { default as useEnhancedEffect } from "./useEnhancedEffect";
+export { default as useForkRef } from "./useForkRef";
+export { default as useYupResolve } from "./useYupResolve";
 export { default as useTheme } from "./useTheme";
 
 
